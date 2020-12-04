e8ad030b4cb410518c396c94fcdf465ec99cfbd6
Michael Webster <miketwebster@gmail.com>
2020-12-04 13:36:30 -0500

    cc-display-panel.c: Allow setting up to 3x scaling.
    This will require a minimum primary resolution of 2100 before
    it becomes available. Any smaller and you start running into
    issues with Cinnamon (with under 700 logical pixels).
    
    This was previously hard-limited to 2x.

--- panels/display/cc-display-panel.c
+++ panels/display/cc-display-panel.c
@@ -51,6 +51,8 @@ CC_PANEL_REGISTER (CcDisplayPanel, cc_display_panel)
 #define MINIMUM_WIDTH 675
 #define MINIMUM_HEIGHT 530
 
+#define MINIMUM_3X_PIXELS 2100
+
 #define INTERFACE_SETTINGS_SCHEMA "org.cinnamon.desktop.interface"
 // #define UPSCALE_SETTINGS_KEY "upscale-fractional-scaling"
 
@@ -1260,6 +1262,7 @@ get_base_scale_string (guint value)
         case 2:
             return g_strdup (_("Double (Hi-DPI)"));
         default:
+            // translate?
             return g_strdup_printf ("%dx", value);
     }
 }
@@ -1318,6 +1321,7 @@ rebuild_base_scale_combo (CcDisplayPanel *self)
   int i, current_base_scale;
   GtkTreeIter selected_iter;
   GtkTreeIter auto_iter, iter;
+  gint primary_height, scale_limit;
 
   model = GTK_LIST_STORE (gtk_combo_box_get_model (GTK_COMBO_BOX (self->priv->base_scale_combo)));
 
@@ -1329,8 +1333,11 @@ rebuild_base_scale_combo (CcDisplayPanel *self)
 
   auto_iter = add_auto_scale_value (self, GTK_TREE_MODEL (model));
 
+  gnome_rr_output_info_get_geometry (self->priv->current_output, NULL, NULL, NULL, &primary_height);
+  scale_limit =  primary_height >= MINIMUM_3X_PIXELS ? 3 : 2;
+
   /* Now add 1 thru max_base_scale, and one past. */
-  for (i = 1; i <= 2; i++)
+  for (i = 1; i <= scale_limit; i++)
   {
     iter = add_base_scale_value (GTK_TREE_MODEL (model), i);
 
